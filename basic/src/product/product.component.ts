543ad5e88ab72d5915a939f6e16a969152c61b3e
Raghavendra Singh <103146867+raghavendra7992@users.noreply.github.com>
2024-03-23 03:05:57 +0530

    search funcanality

--- basic/src/product/product.component.ts
+++ basic/src/product/product.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgComponentOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -10,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
   products = [ { name: 'Laptop', price: 1200, available: true }, { name: 'Smartphone', price: 800, available: true }, { name: 'Headphones', price: 100, available: false }, { name: 'Camera', price: 600, available: true }, { name: 'Tablet', price: 300, available: false }, { name: 'Fitness Tracker', price: 50, available: true }, { name: 'Bluetooth Speaker', price: 80, available: true }, { name: 'Gaming Console', price: 400, available: false }, { name: 'Smartwatch', price: 150, available: true }, { name: 'External Hard Drive', price: 120, available: true }, ];
+
+  searchQuery: string = '';
+  get filteredProducts() {
+    return this.products.filter(product =>
+      product.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
 }
